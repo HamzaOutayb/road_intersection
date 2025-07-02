83dd9e62b70b92dcf2f721a2cab32e4c93581900
houtaib <houtayb@gmail.com>
2025-07-02 11:03:35 +0100

    d

--- src/models/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod animation;
-pub mod car;
-pub mod light;
-pub mod prelude;
-pub mod route;
-pub mod window;
--- src/util/random.rs
+++
@@ -1,6 +0,0 @@
-use crate::models::prelude::*;
-
-pub fn get_random_num(max: i32) -> i32 {
-    let mut rng = rand::rng();
-    rng.random_range(0..=max)
-}
