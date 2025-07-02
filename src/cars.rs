cad0e1f76da5a48662f3e33f0aa0326a1a3e85fb
hkhlifi <khllifihamza66@gmail.com>
2025-07-02 12:59:56 +0100

    almost done

--- src/cars.rs
+++ src/cars.rs
@@ -1,8 +1,11 @@
+use std::borrow;
+use std::cell::RefCell;
+
 use sdl2::{render::Canvas, video::Window};
 
-use sdl2::rect::Rect;
-use sdl2::pixels::Color;
 use rand::Rng;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 
 #[derive(PartialEq, Debug)]
 pub enum TrafficLight {
@@ -13,7 +16,7 @@ pub enum TrafficLight {
 }
 
 impl TrafficLight {
- pub fn change_traffic_light(self) -> Self {
+    pub fn change_traffic_light(self) -> Self {
         match self {
             TrafficLight::UpperRight => TrafficLight::UpperLeft,
             TrafficLight::UpperLeft => TrafficLight::LowerRight,
@@ -25,7 +28,7 @@ impl TrafficLight {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Vehicles {
-    pub vehicles: Vec<Vehicle>
+    pub vehicles: RefCell<Vec<RefCell<Vehicle>>>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -38,125 +41,236 @@ pub struct Vehicle {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Route {
-Left,
-Right,
-Straight,
+    Left,
+    Right,
+    Straight,
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Direction {
-North,
-South,
-East,
-West,
+    North,
+    South,
+    East,
+    West,
 }
 
 impl Vehicles {
     pub fn new() -> Self {
         Vehicles {
-            vehicles: Vec::new(),
+            vehicles: RefCell::new(Vec::new()),
         }
     }
 
     pub fn add_car(&mut self, direction: Direction) {
         let route = random_route();
-        
-        
-       let new_vehicle = match direction {
-        Direction::South => Vehicle {
-            route,
-            direction,
-            x: 350,
-            y: 0,
-        },
-        Direction::North => Vehicle {
-            route,
-            direction,
-            x: 400,
-            y: 650,
-        },
-        Direction::West => Vehicle {
-            route,
-            direction,
-            x: 0,
-            y: 300,
-        },
-        Direction::East => Vehicle {
-            route,
-            direction,
-            x: 800,
-            y: 250,
-        },
-    };
 
-        if self.vehicles.iter().any(|v| intersects(&new_vehicle, v)) {
-        return; 
+        let new_vehicle = match direction {
+            Direction::South => RefCell::new(Vehicle {
+                route,
+                direction,
+                x: 350,
+                y: 0,
+            }),
+            Direction::North => RefCell::new(Vehicle {
+                route,
+                direction,
+                x: 400,
+                y: 650,
+            }),
+            Direction::West => RefCell::new(Vehicle {
+                route,
+                direction,
+                x: 800,
+                y: 250,
+            }),
+            Direction::East => RefCell::new(Vehicle {
+                route,
+                direction,
+                x: 0,
+                y: 300,
+            }),
         };
 
-        self.vehicles.push(new_vehicle)
-}
+        if self
+            .vehicles
+            .borrow()
+            .iter()
+            .any(|v| intersects(&new_vehicle.borrow(), &v.borrow()))
+            // || self.vehicles.borrow().len() >= 8
+        {
+            return;
+        };
 
-    pub fn draw_cars(&mut self, canvas: &mut Canvas<Window>, current_light: &TrafficLight) {
-        if self.vehicles.len() == 0 { return }
-
-        for vehicle in &mut self.vehicles {
-
-        if vehicle.route == Route::Left {
-            canvas.set_draw_color(Color::YELLOW);
-        } else if vehicle.route == Route::Right {
-            canvas.set_draw_color(Color::MAGENTA);
-        } else {
-            canvas.set_draw_color(Color::BLUE);
-        }
-
-        canvas.fill_rect(Rect::new(vehicle.x, vehicle.y,50, 50)).unwrap();
-
-        // stop sign // 
-        if vehicle.direction == Direction::South && vehicle.y==200 &&  *current_light != TrafficLight::UpperLeft {
-            continue
-        }
-
-        if vehicle.direction == Direction::North && vehicle.y ==350 &&  *current_light != TrafficLight::LowerRight {
-            continue
-        }
-        
-        if vehicle.direction == Direction::West && vehicle.x ==300 &&  *current_light != TrafficLight::LowerLeft {
-            continue
-        }
-
-        if vehicle.direction == Direction::East && vehicle.x ==450 &&  *current_light != TrafficLight::UpperRight {
-            continue
-        }
-        
-        if  vehicle.y <= 300 && vehicle.direction == Direction::North && vehicle.route == Route::Right {
-            vehicle.x += 2;
-            // if self.vehicles.clone().iter().any(|v| intersects(&vehicle, v)) {
-            // vehicle.x -= 2; 
-            // };
-        } else if  vehicle.y <= 250 && vehicle.direction == Direction::North && vehicle.route == Route::Left {
-            vehicle.x -= 2
-        } else if  vehicle.y >= 250 && vehicle.direction == Direction::South && vehicle.route == Route::Right {
-            vehicle.x -= 2
-        } else if  vehicle.y >= 300 && vehicle.direction == Direction::South && vehicle.route == Route::Left {
-            vehicle.x += 2
-        } else if vehicle.direction == Direction::North {
-            vehicle.y -= 2;
-        } else if vehicle.direction == Direction::South {
-            vehicle.y += 2;
-        } else if vehicle.direction == Direction::East {
-            vehicle.x -= 2;
-        } else if vehicle.direction == Direction::West {
-            vehicle.x += 2;
-        }
-        }
-
+        self.vehicles.borrow_mut().push(new_vehicle)
     }
 
-    
-        pub fn add_random_car(&mut self) {
-        let mut rng = rand::thread_rng();
+    pub fn draw_cars(&mut self, canvas: &mut Canvas<Window>, current_light: &TrafficLight) {
+        if self.vehicles.borrow().is_empty() {
+            return;
+        }
 
-        let direction = match rng.gen_range(0..4) {
+        for (index, vehicle) in &mut self.vehicles.borrow().iter().enumerate() {
+            if vehicle.borrow().route == Route::Left {
+                canvas.set_draw_color(Color::YELLOW);
+            } else if vehicle.borrow().route == Route::Right {
+                canvas.set_draw_color(Color::MAGENTA);
+            } else {
+                canvas.set_draw_color(Color::BLUE);
+            }
+
+            canvas
+                .fill_rect(Rect::new(vehicle.borrow().x, vehicle.borrow().y, 50, 50))
+                .unwrap();
+
+            // stop sign //
+            if vehicle.borrow().direction == Direction::South
+                && vehicle.borrow().y == 200
+                && *current_light != TrafficLight::UpperLeft
+            {
+                continue;
+            }
+
+            if vehicle.borrow().direction == Direction::North
+                && vehicle.borrow().y == 350
+                && *current_light != TrafficLight::LowerRight
+            {
+                continue;
+            }
+
+            if vehicle.borrow().direction == Direction::West
+                && vehicle.borrow().x == 450
+                && *current_light != TrafficLight::UpperRight
+            {
+                continue;
+            }
+
+            if vehicle.borrow().direction == Direction::East
+                && vehicle.borrow().x == 300
+                && *current_light != TrafficLight::LowerLeft
+            {
+                continue;
+            }
+
+            if vehicle.borrow().x >= 350
+                && vehicle.borrow().direction == Direction::East
+                && vehicle.borrow().route == Route::Right
+            {
+                vehicle.borrow_mut().y += 2;
+            } else if vehicle.borrow().x >= 400
+                && vehicle.borrow().direction == Direction::East
+                && vehicle.borrow().route == Route::Left
+            {
+                vehicle.borrow_mut().y -= 2
+            } else if vehicle.borrow().x <= 400
+                && vehicle.borrow().direction == Direction::West
+                && vehicle.borrow().route == Route::Right
+            {
+                vehicle.borrow_mut().y -= 2
+            } else if vehicle.borrow().x <= 350
+                && vehicle.borrow().direction == Direction::West
+                && vehicle.borrow().route == Route::Left
+            {
+                vehicle.borrow_mut().y += 2
+            }
+            //working
+            else if vehicle.borrow().y <= 300
+                && vehicle.borrow().direction == Direction::North
+                && vehicle.borrow().route == Route::Right
+            {
+                vehicle.borrow_mut().x += 2;
+            } else if vehicle.borrow().y <= 250
+                && vehicle.borrow().direction == Direction::North
+                && vehicle.borrow().route == Route::Left
+            {
+                vehicle.borrow_mut().x -= 2
+            } else if vehicle.borrow().y >= 250
+                && vehicle.borrow().direction == Direction::South
+                && vehicle.borrow().route == Route::Right
+            {
+                vehicle.borrow_mut().x -= 2
+            } else if vehicle.borrow().y >= 300
+                && vehicle.borrow().direction == Direction::South
+                && vehicle.borrow().route == Route::Left
+            {
+                vehicle.borrow_mut().x += 2
+            } else if vehicle.borrow().direction == Direction::North {
+                vehicle.borrow_mut().y -= 2;
+                if self
+                    .vehicles
+                    .borrow()
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| i != &index)
+                    .any(|(_, v)| intersects(&vehicle.borrow(), &v.borrow()))
+                {
+                    vehicle.borrow_mut().y += 2;
+                };
+            } else if vehicle.borrow().direction == Direction::South {
+                vehicle.borrow_mut().y += 2;
+                if self
+                    .vehicles
+                    .borrow()
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| i != &index)
+                    .any(|(_, v)| intersects(&vehicle.borrow(), &v.borrow()))
+                {
+                    vehicle.borrow_mut().y -= 2;
+                };
+            } else if vehicle.borrow().direction == Direction::East {
+                vehicle.borrow_mut().x += 2;
+                if self
+                    .vehicles
+                    .borrow()
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| i != &index)
+                    .any(|(_, v)| intersects(&vehicle.borrow(), &v.borrow()))
+                {
+                    vehicle.borrow_mut().x -= 2;
+                };
+            } else if vehicle.borrow().direction == Direction::West {
+                vehicle.borrow_mut().x -= 2;
+                if self
+                    .vehicles
+                    .borrow()
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| i != &index)
+                    .any(|(_, v)| intersects(&vehicle.borrow(), &v.borrow()))
+                {
+                    vehicle.borrow_mut().x += 2;
+                };
+            }
+            // let (width, height) = canvas.window().size();
+            // let mut borrowed = self.vehicles.borrow_mut();
+            // match vehicle.borrow().direction {
+            //     Direction::North => {
+            //         if vehicle.borrow().y < 0 {
+            //             borrowed.remove(index);
+            //         }
+            //     }
+            //     Direction::South => {
+            //         if vehicle.borrow().y > height as i32 {
+            //             borrowed.remove(index);
+            //         }
+            //     }
+            //     Direction::East => {
+            //         if vehicle.borrow().x > width as i32 {
+            //             borrowed.remove(index);
+            //         }
+            //     }
+            //     Direction::West => {
+            //         if vehicle.borrow().x < 0 {
+            //             borrowed.remove(index);
+            //         }
+            //     }
+            // }
+        }
+    }
+
+    pub fn add_random_car(&mut self) {
+        let direction = match get_random_num(3) {
             0 => Direction::North,
             1 => Direction::South,
             2 => Direction::East,
@@ -168,13 +282,17 @@ impl Vehicles {
 }
 
 fn random_route() -> Route {
-    let rng = rand::thread_rng().gen_range(0..3);
-    match rng {
-        0 => return Route::Left,
-        1=> return Route::Right,
-        2=> return Route::Straight,
-        _=> unreachable!(),
+    match get_random_num(2) {
+        0 => Route::Left,
+        1 => Route::Right,
+        2 => Route::Straight,
+        _ => unreachable!(),
     }
+}
+
+pub fn get_random_num(max: i32) -> i32 {
+    let mut rng = rand::rng();
+    rng.random_range(0..=max)
 }
 
 fn intersects(a: &Vehicle, b: &Vehicle) -> bool {
@@ -190,8 +308,10 @@ fn intersects(a: &Vehicle, b: &Vehicle) -> bool {
     let b_top = b.y;
     let b_bottom = b.y + size;
 
-    !(a_left >= b_right ||
-      a_right <= b_left ||
-      a_top >= b_bottom ||
-      a_bottom <= b_top)
+    !(a_left >= b_right
+        || a_right <= b_left
+        || a_top >= b_bottom
+        || a_bottom <= b_top
+        || a.direction != b.direction
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::{Duration, Instant};
-use sdl2::{render::Canvas, video::Window};
+use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
+use sdl2::{render::Canvas, video::Window};
+use std::time::{Duration, Instant};
 
 mod cars;
-use crate::cars::{TrafficLight, Vehicles, Direction};
+use crate::cars::{Direction, TrafficLight, Vehicles};
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("road_intersection", 800, 600)
+    let window = video_subsystem
+        .window("road_intersection", 800, 600)
         .position_centered()
         .build()
         .unwrap();
@@ -27,25 +28,28 @@ fn main() {
 
     let mut traffic_light = TrafficLight::UpperRight;
     let mut last_change = Instant::now();
-    let change_interval = Duration::from_secs(3);
+    let change_interval = Duration::from_secs(5);
 
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
-                | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'running;
                 }
-                Event::KeyDown { keycode: Some(key), .. } => {
-                    match key {
-                        Keycode::Up => all_vehicles.add_car(Direction::North),
-                        Keycode::Down => all_vehicles.add_car(Direction::South),
-                        Keycode::Left => all_vehicles.add_car(Direction::East),
-                        Keycode::Right => all_vehicles.add_car(Direction::West),
-                        Keycode::R => all_vehicles.add_random_car(),
-                        _ => {}
-                    }
-                }
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => match key {
+                    Keycode::Up => all_vehicles.add_car(Direction::North),
+                    Keycode::Down => all_vehicles.add_car(Direction::South),
+                    Keycode::Left => all_vehicles.add_car(Direction::West),
+                    Keycode::Right => all_vehicles.add_car(Direction::East),
+                    Keycode::R => all_vehicles.add_random_car(),
+                    _ => {}
+                },
                 _ => {}
             }
         }
@@ -78,20 +82,56 @@ fn draw_roads(canvas: &mut Canvas<Window>, current_light: &TrafficLight) {
 
     // Road lines
     canvas.set_draw_color(Color::WHITE);
-    canvas.draw_line(Point::new(center_x, 0), Point::new(center_x, height)).unwrap();
-    canvas.draw_line(Point::new(center_x + 50, 0), Point::new(center_x + 50, height)).unwrap();
-    canvas.draw_line(Point::new(center_x - 50, 0), Point::new(center_x - 50, height)).unwrap();
+    canvas
+        .draw_line(Point::new(center_x, 0), Point::new(center_x, height))
+        .unwrap();
+    canvas
+        .draw_line(
+            Point::new(center_x + 50, 0),
+            Point::new(center_x + 50, height),
+        )
+        .unwrap();
+    canvas
+        .draw_line(
+            Point::new(center_x - 50, 0),
+            Point::new(center_x - 50, height),
+        )
+        .unwrap();
 
-    canvas.draw_line(Point::new(0, center_y), Point::new(width, center_y)).unwrap();
-    canvas.draw_line(Point::new(0, center_y + 50), Point::new(width, center_y + 50)).unwrap();
-    canvas.draw_line(Point::new(0, center_y - 50), Point::new(width, center_y - 50)).unwrap();
+    canvas
+        .draw_line(Point::new(0, center_y), Point::new(width, center_y))
+        .unwrap();
+    canvas
+        .draw_line(
+            Point::new(0, center_y + 50),
+            Point::new(width, center_y + 50),
+        )
+        .unwrap();
+    canvas
+        .draw_line(
+            Point::new(0, center_y - 50),
+            Point::new(width, center_y - 50),
+        )
+        .unwrap();
 
     // Traffic lights positions
     let positions = [
-        (TrafficLight::UpperRight, Rect::new(center_x + 50, center_y - 100, 50, 50)),
-        (TrafficLight::UpperLeft, Rect::new(center_x - 100, center_y - 100, 50, 50)),
-        (TrafficLight::LowerRight, Rect::new(center_x + 50, center_y + 50, 50, 50)),
-        (TrafficLight::LowerLeft, Rect::new(center_x - 100, center_y + 50, 50, 50)),
+        (
+            TrafficLight::UpperRight,
+            Rect::new(center_x + 50, center_y - 100, 50, 50),
+        ),
+        (
+            TrafficLight::UpperLeft,
+            Rect::new(center_x - 100, center_y - 100, 50, 50),
+        ),
+        (
+            TrafficLight::LowerRight,
+            Rect::new(center_x + 50, center_y + 50, 50, 50),
+        ),
+        (
+            TrafficLight::LowerLeft,
+            Rect::new(center_x - 100, center_y + 50, 50, 50),
+        ),
     ];
 
     // Draw traffic lights, green for active, red otherwise
